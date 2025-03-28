9b1c7b664c22cdeffde85a9cca7208c659874e93
Chris Ryan <117776119@umail.ucc.ie>
2025-03-28 19:37:58 +0000

    updated

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ module.exports = merge(common, {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    watchFiles: ['./src/templates/template.html'],
     hot: true, // Enables Hot Module Replacement
     open: true, // Automatically opens the browser
     static: './dist',
